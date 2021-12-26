321f794f57de8fcf575c8cdc484a5f8f85cb1308
latifpro <latif15-11375@diu.edu.bd>
2021-12-27 02:07:33 +0600

    final commit

---
+++ app/src/main/java/com/example/hello/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText emailBox, passwordBox;
+    Button loginBtn, signupBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailBox = findViewById(R.id.emailBox);
+        passwordBox = findViewById(R.id.passwordBox);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signupBtn =findViewById(R.id.createBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+
+            }
+        });
+    }
+}
